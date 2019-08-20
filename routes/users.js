747573aab5689a20542c4f01644a5a6dc53afd81
mely <meli.bermudez@yahoo.es>
2019-08-20 08:09:18 -0300

    ajustando para hacer el alta de usuario

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../bin/mysql'); 
+/* const bcrypt = require('bcrypt'); 
+const jwt = require('jsonwebtoken'); */
 
 /* GET users listing. */
-router.get('/signin', function(req, res, next) {
+/* router.get('/signin', function(req, res, next) {
   res.render('registro', { title: 'Registro de usuario' });
-});
+});  */
 
 /** ALTA DE USUARIO */
-router.post('signin', function (req, res, next) {
-  db.query("INSERT INTO usuarios (nombre, email, pass) VALUES (?, ?, ?)",[req.body.nombre, req.body.mail, req.body.password ], function (error, result) {
+router.post('/signin', function (req, res, next) {
+  db.query("INSERT INTO contactos (nombre, email, password, calle, altura_depto, ciudad, provincia, cod_postal) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",[req.body.nombre, req.body.email, req.body.password, req.body.calle, req.body.altura, req.body.ciudad, req.body.provincia, req.body.postal], function (error, result) {
       console.log(error, result);
       console.log(req.body)
   }
   );
-  res.send('gracias por su mensaje');
   console.log(req.body)
 });
 
